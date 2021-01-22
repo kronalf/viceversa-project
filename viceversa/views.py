95f8da5dfae766389d84a761f3cf302336d30e67
Ковалев Дмитрий Георгиевич <kovalev_dg@minudo-home.ru>
2021-01-22 15:08:16 +0300

    add new function

--- viceversa/views.py
+++ viceversa/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
+
+
+def reverse(request):
+    user_text = request.GET['usertext']
+    number_words = user_text.split(' ')
+    count = len(number_words)
+    if count > 1:
+        s = 's'
+    else:
+        s = ''
+    reverse_text = user_text[::-1]
+    return render(request, 'reverse.html', {'usertext':user_text, 'reversetext':reverse_text, 'count':count, 's':s})
